74699c28f505db3a1f37817b7cdbba9a1e86695f
Kethan Pilla <90024966+KethanPilla@users.noreply.github.com>
2024-11-06 20:56:29 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 $("button").click(function(){
-    $.getJSON("https://github.com/KethanPilla/dev-george-lab/scripts/data.json", function(result){
+    $.getJSON("https://github.com/KethanPilla/dev-george-lab/blob/main/scripts/data.json", function(result){
       $.each(result, function(i, field){
         $("div").append(field + " ");
       });
